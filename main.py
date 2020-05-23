e05458acd96250c566760d901dfd68abb91dba60
twisted <youdangls@gmail.com>
2020-05-23 10:54:14 +0800

    更新信息分类判断条件

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-
+import re
 import logging
 from database.db_operator import DbOperator
 from ob import Observer
 from my_timer import RepeatedTimer
 
-
-CMD_LIST = ["status", "list", "admin-status", "admin-list"]
+EMAIL_RULE = re.compile(r'^[a-zA-Z0-9\._\-\+]{1,64}@([A-Za-z0-9_\-\.]){1,128}\.([A-Za-z]{2,8})$')
+CMD_LIST = ["help", "status", "list", "admin-status", "admin-list"]
 class App(object):
     def __init__(self):
         self.db    = DbOperator()
@@ -38,11 +38,24 @@ class App(object):
     def _handle_cmd(self, msg):
         pass
 
+
+
+
+
+
     def _is_URL(self, string):
+        # 特殊处理，暂时只接受微信文章地址
+        if string.startswith('https://mp.weixin.qq.com/s'):
+            #合法性校验
+            if (' ' not in string) and (len(string) < 2000):
+                return True
         return False
 
     def _is_email(self, string):
-        return False
+        return EMAIL_RULE.match(string)
 
     def _is_cmd(self, string):
-        return False
+        if string in CMD_LIST:
+            return True
+        else:
+            return False
